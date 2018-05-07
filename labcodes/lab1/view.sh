8aa7703466cfdcb6761ec5d2f989a693b0d0fa84
FluorineDog <fluorine@gmail.com>
2018-05-07 12:58:23 +0800

    update

--- labcodes/lab1/view.sh
+++ labcodes/lab1/view.sh
@@ -1,8 +1,8 @@
 make clean &&
-make --trace &> makeing.log &&
+make --trace &> making.log &&
 cat making.log \
 | grep -v -E "(update target|warning:|^ |^echo|^Makefile|^mkdir)"  \
-| grep -E "(^\+ |^\+ ld|^ld|^gcc|^objdump|^dd|^objcopy|^#)"
+# | grep -E "(^\+ |^\+ ld|^ld|^gcc|^objdump|^dd|^objcopy|^#)"
 
 #| grep -E "(^ld)"   \
 #| grep -E "(^gcc)"   \
